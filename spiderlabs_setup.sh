92b58008aaeb96534c0b9f81c9a9a9d5b2c32484
Joshua Skorich <jskorich@trustwave.com>
2015-05-27 21:11:12 -0500

    fixed stupid sed idiosyncracies for linux

--- spiderlabs_setup.sh
+++ spiderlabs_setup.sh
@@ -2,6 +2,6 @@
 
 # Setup script for scepwn-ng for SpiderLabs default virtual machine
 
-sed -i '' "s/WINEXE = .*/WINEXE = \"\/pentest\/SpiderLabs\/winexe-PTH\"/" scepwn-ng.rb
-sed -i '' "s/SCE = .*/SCE = \"\/pentest\/SpiderLabs\/Win-Tools\/sce.32.exe\"/" scepwn-ng.rb
+sed -i'' "s/WINEXE = .*/WINEXE = \"\/pentest\/SpiderLabs\/winexe-PTH\"/" scepwn-ng.rb
+sed -i'' "s/SCE = .*/SCE = \"\/pentest\/SpiderLabs\/Win-Tools\/sce.32.exe\"/" scepwn-ng.rb
 
